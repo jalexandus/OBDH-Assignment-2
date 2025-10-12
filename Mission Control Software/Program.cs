906387470ee567ed69b901e85ed05042d462519b
jalexandus <jonathanalexandus@gmail.com>
2025-10-12 13:45:21 +0200

    Add MCS software and json files

---
+++ Mission Control Software/Program.cs
@@ -0,0 +1,11 @@
+﻿namespace HelloWorld;
+
+internal class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("██████████████████████████████████████████████████████████████████");
+        Console.WriteLine("████████████████ ~ LTU Mission Control software ~ ████████████████");
+        Console.WriteLine("██████████████████████████████████████████████████████████████████");
+    }
+}
